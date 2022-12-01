5bd782e8df1b50d7e3f25eecb1ba59d3faedd8bd
suraj <surajn097@gmail.com>
2022-12-01 22:09:31 +0530

    addfile

--- Main File/create_instance.tf
+++ Main File/create_instance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIA2ZBYHKFR3E6WLNSD"
-  secret_key = ""
-  region = "us-east-2"
-}
-
 
 resource "aws_instance" "MyAWS" {
   ami           = "ami-05803413c51f242b7"
---
+++ Main File/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIA2ZBYHKFR3E6WLNSD"
+  secret_key = ""
+  region = "us-east-2"
+}
