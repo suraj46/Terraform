d1e20c9e98a585751e1cf4c01965e59e9e9ff6c5
suraj <surajn097@gmail.com>
2022-12-03 15:53:23 +0530

    addfile

--- Main File/create_instance.tf
+++ Main File/create_instance.tf
@@ -1,12 +1,7 @@
 
 resource "aws_instance" "MyAWS" {
-  count = 3
   ami           = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
 
-  tags = {
-
-    name = "demoinstance-${count.index}"
-  }
 
 }
